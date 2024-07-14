18bb28e08ace26484bb96e4b98ee268c3181e29f
Mixas75 <102125428+Mixas75@users.noreply.github.com>
2024-07-14 15:00:03 -0300

    exercícios JS

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 58;
+
+console.log(`Olá, a sua velocidadde é ${vel} km/h`);
+
+if (vel > 60) {
+    console.log("Você foi MULTADO!")
+}
+console.log("Dirija com cautela!");
---
+++ Aula11/ex009.js
@@ -0,0 +1,11 @@
+let pais = "BRASIL";
+console.log(`País de nascimento: ${pais}`);
+if (pais == "BRASIL") {
+    console.log(
+        "Você é brasileira(o)"
+    )
+} else {
+    console.log(
+        "Você não é brasileira(o)"
+    )
+}
