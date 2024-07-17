9f95c5cff690e132514a0572878727af81b0cefb
Michel Manhães <102125428+Mixas75@users.noreply.github.com>
2024-07-17 20:05:42 -0300

    Verificador de idade e faixa etária
    Projeto para verificar a idade a partir da data de nascimento e apresentar uma foto de acordo com a faixa etária.

--- Aula012-exercicios/ex015/script.js
+++ Aula012-exercicios/ex015/script.js
@@ -8,6 +8,7 @@ function verificar() {
   let dataNasc = new Date(dataNascValue);
   let res = document.querySelector("div#res");
   let genero = "";
+  let faixa = "";
 
   if (dataNasc > data) {
     alert("Erro! Data de nascimento não pode ser maior que a data atual.");
@@ -22,7 +23,8 @@ function verificar() {
     let diaNasc = dataNasc.getDate();
     let idade = ano - anoNasc;
     let img = document.createElement('img');
-    img.setAttribute('id', 'foto')
+    img.setAttribute('id', 'foto');
+    img.setAttribute('alt', 'Foto do gênero');
 
     if (mes < mesNasc){
         idade--;
@@ -32,12 +34,46 @@ function verificar() {
     }
     if (fsex[0].checked){
         genero = "homem";
-    }
+        if (idade >= 0 && idade < 12) {
+            img.setAttribute('src', 'crianca-menino.png')
+            faixa = "Criança";
+          } else if (idade < 18) {
+            img.setAttribute('src', 'adolescentem.png')
+            faixa = "Adolescente";
+          }
+          else if (idade < 60) {
+            img.setAttribute('src', 'adulto.png')
+            faixa = "Adulto";
+          } else {
+            img.setAttribute('src', 'idoso.png')
+            faixa = "Idoso";
+          }
+        }
     else if (fsex[1].checked){
         genero = "mulher";
+        if (idade >= 0 && idade < 12) {
+            img.setAttribute('src', 'crianca-menina.png')
+            faixa = "Criança";
+          } else if (idade < 18) {
+            img.setAttribute('src', 'adolescentef.png')
+            faixa = "Adolescente";
+          }
+          else if (idade < 60) {
+            img.setAttribute('src', 'adulta.png')
+            faixa = "Adulta";
+          } else {
+            img.setAttribute('src', 'idosa.png')
+            faixa = "Idosa";
+          }
     }
-    res.style.textAllign = 'center';
-    res.innerHTML = `Idade calculada: ${idade} anos. Gênero detectado: ${genero}`
+    res.style.textAlign = 'center';
+    res.innerHTML = "";
+   
+    let faixaText = document.createTextNode(`${idade} anos: ${faixa}`);
+    let p = document.createElement('p');
+    p.appendChild(faixaText);
+    res.appendChild(p);
+    res.appendChild(img);
     }
 }
 
