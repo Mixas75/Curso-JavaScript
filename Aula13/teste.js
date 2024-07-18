a2f6be97c481305d5df258797d788e33e13c833d
Michel Manhães <102125428+Mixas75@users.noreply.github.com>
2024-07-17 22:45:31 -0300

    Estruturas de repetição
    Estudos iniciais: while

---
+++ Aula13/teste.js
@@ -0,0 +1,12 @@
+/*console.log("Olá");
+console.log("Olá");
+console.log("Olá");
+console.log("Olá");
+console.log("Olá");
+console.log("Olá");*/
+
+let cont = 1;
+while(cont <= 6){
+    console.log(`Passo ${cont}`)
+    cont++
+}
