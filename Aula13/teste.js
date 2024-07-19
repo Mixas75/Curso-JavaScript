10c8010e25a76307c60119dadc3a77a8f4f38938
Michel Manhães <102125428+Mixas75@users.noreply.github.com>
2024-07-18 21:21:26 -0300

    Repetições
    Estruturas While e Do While

--- Aula13/teste.js
+++ Aula13/teste.js
@@ -5,8 +5,14 @@ console.log("Olá");
 console.log("Olá");
 console.log("Olá");*/
 
-let cont = 1;
+/*let cont = 1;
 while(cont <= 6){
     console.log(`Passo ${cont}`)
     cont++
-}
+}*/
+
+let cont = 1;
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while(cont <= 7)
